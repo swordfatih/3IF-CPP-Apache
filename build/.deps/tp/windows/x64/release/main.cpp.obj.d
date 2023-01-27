56237080ad6eb9cff5a9a7991eb51c8115207560
swordfatih <accfldekur@gmail.com>
2023-01-27 14:52:57 +0100

    squelette

---
+++ build/.deps/tp/windows/x64/release/main.cpp.obj.d
@@ -0,0 +1,14 @@
+{
+    depfiles_gcc = "main.o: main.cpp\
+",
+    files = {
+        "main.cpp"
+    },
+    values = {
+        [[C:\msys64\mingw64\bin\x86_64-w64-mingw32-g++]],
+        {
+            "-std=c++11",
+            "-fexceptions"
+        }
+    }
+}
---
+++ build/.deps/tp/windows/x64/release/tp.exe.d
@@ -0,0 +1,9 @@
+{
+    files = {
+        [[build\.objs\tp\windows\x64\release\main.cpp.obj]]
+    },
+    values = {
+        [[C:\msys64\mingw64\bin\x86_64-w64-mingw32-g++]],
+        { }
+    }
+}
